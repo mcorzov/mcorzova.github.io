7c26c3e1fc5ff5897d9440b3a7abf721c61f1a7a
mcorzov <146891924+mcorzov@users.noreply.github.com>
2025-02-20 23:40:12 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,4 @@
+document.write("INFORMES DE LABORATORIOS DE DENGUE");
+alert("BIENVENIDO");
+var edad= prompt ("¿Cual es tu edad?");
+if (edad >=18) {alert("Eres mayor de Edad");} else {("Eres menor de Edad");}
